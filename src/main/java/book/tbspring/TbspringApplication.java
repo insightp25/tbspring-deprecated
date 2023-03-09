395323a1ec04e5fbfb2a3fa25f5142d2bac1d6f3
insightp25 <insightplug@gmail.com>
2023-03-09 19:30:52 +0900

    first commit

---
+++ src/main/java/book/tbspring/TbspringApplication.java
@@ -0,0 +1,13 @@
+package book.tbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/book/tbspring/TbspringApplicationTests.java
@@ -0,0 +1,13 @@
+package book.tbspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
